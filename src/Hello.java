27871c94b72e9083c86dc0a32a0a0c7267c183d0
jscha <jscha@Asus_G15.unl.edu>
2022-08-25 11:50:00 -0500

    Added my major

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+/*
+ * Author: 	Jacob Schager
+ * 			jschager3@huskers.unl.edu
+ * Date: 	2022/08/25
+ * 
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Jacob\n" + "Computer Science");
+
+	}
+
+}
